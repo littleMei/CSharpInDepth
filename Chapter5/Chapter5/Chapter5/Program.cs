9c25cf277f5a8516803d939665f4fa333c1a1ae1
Danyi Luo <d.luo@allegrodevelopment.com>
2018-01-31 14:20:42 -0600

    commit0131

---
+++ Chapter5/Chapter5/Chapter5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter5
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Button button = new Button();
+            button.Text = "Click Me";
+            button.Click += new EventHandler(LogPlainEvent);
+            button.KeyPress += new KeyPressEventHandler(LogKeyEvent);
+            button.MouseClick += new MouseEventHandler(LogMouseEvent);
+
+            Form form = new Form();
+            form.AutoSize = true;
+            form.Controls.Add(button);
+
+            Application.Run(new form());
+        }
+
+        static void LogPlainEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("LogPlain");
+        }
+
+        static void LogKeyEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("LogKey");
+        }
+
+        static void LogMouseEvent(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("LogMouse");
+        }
+    }
+}
